d933b7b35b6ce8dc789b14d88dced21b26d442ed
santoshchavva <santosh_chavva@yahoo.co.in>
2024-10-09 10:44:20 +0530

    Added Apache POI dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/org.apache.poi/poi
+    implementation("org.apache.poi:poi:5.3.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
-rootProject.name = "bsnl-excel-extractor"
+rootProject.name = "bsnl-list-generator"
 
