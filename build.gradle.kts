2827a62c1a0946981dda3a152ee54fe6ff684068
santoshchavva <santosh_chavva@yahoo.co.in>
2024-11-02 17:20:24 +0530

    Added Apache POI Read and write

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,10 @@ repositories {
 dependencies {
     // https://mvnrepository.com/artifact/org.apache.poi/poi
     implementation("org.apache.poi:poi:5.3.0")
-
+// https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml
+    implementation("org.apache.poi:poi-ooxml:5.3.0")
+    implementation 'org.apache.logging.log4j:log4j-api:2.17.1'
+    implementation 'org.apache.logging.log4j:log4j-core:2.17.1'
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,70 @@
 package org.example;
+import java.io.*;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
+import org.apache.commons.math3.util.Pair;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//@SuppressWarnings("deprecation")
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException,FileNotFoundException {
+        Pair<String, String> pair = new Pair<>("","");
+        pair.getKey();
+        List<Pair<String,String>> list = new ArrayList<>();
+        list.add(new Pair<>("",""));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String path = "/Users/santoshchavva/Downloads/BSNL/revenueshare.xlsx";
+        File fileObj = new File(URLDecoder.decode(path, StandardCharsets.UTF_8));
+        FileInputStream file=new FileInputStream(fileObj);
+        HashMap<String,String> data=new HashMap<>();
+        XSSFWorkbook reading=new XSSFWorkbook(file);
+        XSSFWorkbook writing=new XSSFWorkbook();
+        XSSFSheet readSheet=reading.getSheetAt(0);
+        XSSFSheet writesheet=writing.createSheet("generated list");
+        Iterator<Row> rowIterator=readSheet.iterator();
+       while (rowIterator.hasNext()){
+            Row row= rowIterator.next();
+            Cell serviceNumber=row.getCell(6);
+            Cell revenueShare=row.getCell(28);
+            String key=getCellValue(serviceNumber);
+            String value=getCellValue(revenueShare);
+            if (key!=null && value!=null){
+                data.put(key,value);
+
+            }
+        }
+        Set<String> keyset=data.keySet();
+        int rownum=0;
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            Row row = writesheet.createRow(rownum++);
+            Cell keyCell = row.createCell(0);
+            keyCell.setCellValue(entry.getKey());
+            Cell valueCell = row.createCell(1);
+            valueCell.setCellValue(entry.getValue());
+        }
+        FileOutputStream fileOutputStream=new FileOutputStream(new File("/Users/santoshchavva/Downloads/BSNL/generated list.xlsx"));
+        writing.write(fileOutputStream);
+        fileOutputStream.close();
+        file.close();
+        data.forEach((k, v) -> System.out.println("Key: " + k + ", Value: " + v));
+        System.out.println(data.size());
+    }
+
+    private static String getCellValue(Cell cell) {
+        if(cell.getCellType()== CellType.NUMERIC) {
+            return String.valueOf(cell.getNumericCellValue());
+        }
+        else if (cell.getCellType()==CellType.STRING){
+            return cell.getStringCellValue();
+        }
+        else{
+            return null;
+
         }
     }
 }
